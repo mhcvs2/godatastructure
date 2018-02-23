ad6f4831bc7599c5b31f82e7eb4111e35c5020a1
Hongchao Ma <hongchao.ma@samsung.com>
2018-02-23 17:52:59 +0800

    add

--- csvFile/csv.go
+++ csvFile/csv.go
@@ -1,14 +1,18 @@
 package csvFile
 
 import (
-	"github.com/mhcvs2/godatastructure/path"
+	"github.com/mhcvs2/godatastructure/util"
 	"os"
 	"encoding/csv"
+	"sync"
 )
 
 type CSV struct {
 	path string
 	initData []string
+	writer *csv.Writer
+	rwLock sync.RWMutex
+	f *os.File
 }
 
 func NewCSVFile(path string) *CSV {
@@ -16,23 +20,34 @@ func NewCSVFile(path string) *CSV {
 }
 
 func (c *CSV) Init(initData ...string) error {
-	if exist, err := path.Exists(c.path); err != nil {
+	if exist, err := util.Exists(c.path); err != nil {
 		return err
 	} else if exist {
+		if c.writer != nil {
+			return nil
+		}
+		if f, err := os.OpenFile("/root/github/go/src/csv/test.csv", os.O_WRONLY, 0644); err !=nil {
+			return err
+		} else {
+			c.f = f
+			c.writer = csv.NewWriter(f)
+		}
 		return nil
 	} else if f, err := os.Create(c.path); err != nil {
 		return err
 	} else {
-		defer f.Close()
+		c.rwLock.Lock()
+		defer c.rwLock.Unlock()
 		f.WriteString("\xEF\xBB\xBF") // 写入UTF-8 BOM
 		w := csv.NewWriter(f)//创建一个新的写入文件流
-		defer w.Flush()
 		if len(initData) > 0 {
 			w.Write(initData)
 			c.initData = initData
 		} else if len(c.initData) > 0 {
 			w.Write(c.initData)
 		}
+		c.writer = w
+		c.f = f
 	}
 	return nil
 }
@@ -41,15 +56,9 @@ func (c *CSV) Write(data ...string) error {
 	if err := c.Init(); err !=nil {
 		return err
 	}
-	if f, err := os.Open(c.path); err !=nil {
-		return err
-	} else {
-		defer f.Close()
-		f.WriteString("\xEF\xBB\xBF") // 写入UTF-8 BOM
-		w := csv.NewWriter(f)//创建一个新的写入文件流
-		defer w.Flush()
-		w.Write(data)
-	}
+	c.rwLock.Lock()
+	defer c.rwLock.Unlock()
+	c.writer.Write(data)
 	return nil
 }
 
@@ -57,14 +66,15 @@ func (c *CSV) WriteAll(data [][]string) error {
 	if err := c.Init(); err !=nil {
 		return err
 	}
-	if f, err := os.Open(c.path); err !=nil {
-		return err
-	} else {
-		defer f.Close()
-		f.WriteString("\xEF\xBB\xBF") // 写入UTF-8 BOM
-		w := csv.NewWriter(f)//创建一个新的写入文件流
-		defer w.Flush()
-		w.WriteAll(data)
-	}
+	c.rwLock.Lock()
+	defer c.rwLock.Unlock()
+	c.writer.WriteAll(data)
 	return nil
+}
+
+func (c *CSV) Done() {
+	c.rwLock.Lock()
+	defer c.rwLock.Unlock()
+	c.writer.Flush()
+	c.f.Close()
 }
